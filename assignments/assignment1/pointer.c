bfbfe7f2cf283e499d9835502e9a5f43f695c824
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2022-11-28 01:44:28 +0530

    Create pointer.c

---
+++ assignments/assignment1/pointer.c
@@ -0,0 +1,21 @@
+https://www.hackerrank.com/challenges/pointer-in-c/problem?isFullScreen=true
+
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function 
+    *a=*a+*b;
+    *b=abs(*a-(2**b));   
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
