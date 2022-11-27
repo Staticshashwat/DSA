0b2fcd6c4ebd19ba4922a9bee1274661ae2f8606
Staticshashwat <118439884+Staticshashwat@users.noreply.github.com>
2022-11-28 01:39:41 +0530

    Create playwithchar.c

---
+++ assignments/assignment1/playwithchar.c
@@ -0,0 +1,21 @@
+https://www.hackerrank.com/challenges/playing-with-characters/forum
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+   char c,s[15],a[100];
+   scanf("%c",&c);
+   scanf("%s",s);
+   getchar();
+   gets(a);
+   printf("%c\n",c);
+   printf("%s\n",s);
+   printf("%s",a);
+       
+    return 0;
+}
